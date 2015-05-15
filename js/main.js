59426d425226d76f3754630a6364a2451e0a098e
Shawn Gibson <saljgib@gmail.com>
2015-05-15 15:00:20 -0400

    updated styling

--- js/main.js
+++ js/main.js
@@ -7,13 +7,3 @@ function alertButtonValue(event) {
 [].forEach.call(document.querySelectorAll('.word'), function(element){
   element.addEventListener('click', alertButtonValue);
 }, false);
-
-function alertNumberPlusNumber(event) {
-  var button = event.target;
-  var number = Number(button.textContent);
-  alert(number + number);
-}
-
-[].forEach.call(document.querySelectorAll('.number'), function(element){
-  element.addEventListener('click', alertNumberPlusNumber);
-}, false);
